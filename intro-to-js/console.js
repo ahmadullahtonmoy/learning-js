49501c9685969d9a6a6e0eb0ba34218815a82468
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-22 02:12:08 +0600

    console.js updated

--- intro-to-js/console.js
+++ intro-to-js/console.js
@@ -5,3 +5,13 @@
 */
 
 console.log(7);
+
+// i can math in console.log;
+console.log(2+2);
+console.log(4-2);
+
+// i'm adding more example;
+console.log("This is addition of 4 and 3 =",4+3);
+console.log("This is subtraction of 10 and 3 =",10-3);
+console.log("This is multiplication of 4 and 3 =",4*2);
+console.log("This is division of 16 and 2 =",16/2);
