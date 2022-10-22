afcb08ba7f4a279097390cfd3152fb8be54ad4cf
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-23 01:27:50 +0600

    fifth updated

--- intro-to-js/fifth.js
+++ intro-to-js/fifth.js
@@ -1,1 +1,6 @@
 // Simple Mathematical operations in JavaScrip
+// modulus
+var mangoes = 7;
+var hungryPeople = 2;
+var divide = mangoes % hungryPeople;
+console.log(divide);
--- intro-to-js/mathematical-operation.js
+++
@@ -1,5 +0,0 @@
-// modulus
-var mangoes = 7;
-var hungryPeople = 2;
-var divide = mangoes % hungryPeople;
-console.log(divide);
