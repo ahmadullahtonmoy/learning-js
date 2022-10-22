4bea36c78a397e8e57dac1d2108db303457ee306
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-23 04:37:58 +0600

    fifth .js updated with mathematical works

--- intro-to-js/fifth.js
+++ intro-to-js/fifth.js
@@ -1,4 +1,26 @@
 // Simple Mathematical operations in JavaScrip
+
+// some number valued variable for mathematic.
+var firstNumber = 12;
+var secondNumber = 6;
+var thirdNumber = 4;
+var fourthNumber = 3;
+var fifthNumber = 2;
+
+// addition with numeric variable.
+var addition = thirdNumber + fifthNumber;
+
+// subtraction
+var subtraction = firstNumber - secondNumber;
+
+// multiplication
+var multiplication = secondNumber * fifthNumber;
+
+// division
+var division = firstNumber / fifthNumber;
+
+
+
 // modulus
 var mangoes = 7;
 var hungryPeople = 2;
