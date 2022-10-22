2699c72ad398ba64dc7b3128f52644f7ff701980
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-23 04:27:56 +0600

    second.js updated with some variable declaretion

--- intro-to-js/second.js
+++ intro-to-js/second.js
@@ -1,1 +1,15 @@
 // What is variable, five things you need to declare a variable
+// numeric variable
+var henPrice = 55;
+var tomAge = 15;
+var temperature = 32;
+
+// string variable
+var firstName = "Tom";
+var lastName = "Cruise";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// boolean variable
+var isDay = false;
+var isNight = true;
