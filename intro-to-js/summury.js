ad45091dfcd68e094c63b124f4c2dbb4648d685b
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-22 23:43:33 +0600

    summury.js updated

--- intro-to-js/summury.js
+++ intro-to-js/summury.js
@@ -1,2 +1,17 @@
-// how to declare a variable.
+// how to declare a variable?
 var exampleVariable = 25;
+
+// how to set value of a variable for second time?
+exampleVariable = 27;
+
+// how to know type of variable?
+var typeofExampleVariable = typeof exampleVariable;
+
+// what is string type variable and how to write code for that?
+var firstName = "Tom";
+var lastName = "Cruise";
+
+// what is boolean type variable and how to declare that?
+var isHotDay = false;
+var isPeacefulDay = true;
+
