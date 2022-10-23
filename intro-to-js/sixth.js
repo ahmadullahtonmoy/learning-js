81d75c1cfb53e48d47facc9b541d61c65860ef89
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-23 10:45:58 +0600

    mathematical shorthand added in sixth.js

--- intro-to-js/sixth.js
+++ intro-to-js/sixth.js
@@ -1,1 +1,15 @@
 // (advanced) Mathematical operation shorthand
+var tomatoPrice = 6;
+var potatoPrice = 4;
+
+// addition shorthand
+potatoPrice = potatoPrice + 2;
+potatoPrice += 2;
+potatoPrice ++;
+console.log(potatoPrice);
+
+// subtraction shorthand
+tomatoPrice = tomatoPrice - 2;
+tomatoPrice -= 2;
+tomatoPrice --;
+console.log(tomatoPrice);
