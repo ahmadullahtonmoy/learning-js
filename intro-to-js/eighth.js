9c9222899b7e7c5cff0e8ad9d62f06f45793f783
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-23 11:03:22 +0600

    eighth.js updated

--- intro-to-js/eighth.js
+++ intro-to-js/eighth.js
@@ -1,4 +1,9 @@
 // Different variable types and use toFixed with parseFloat.
 
-// i thought that there are only three types of variable. but there are more two types
-/*  all types of variable: 1- Numerical 2- String 3- Boolean 4- Undefined 5- Null */
+// i thought that there are only three types of variable. but there are mso many data types in js.
+/*  some types of variable: 1- Numerical 2- String 3- Boolean 4- Undefined 5- Null */
+
+// undefined
+var undefined;
+
+console.log(undefined);
