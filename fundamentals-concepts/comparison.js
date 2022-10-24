ca0d132d464490543e060a8f83c16e58687b03d7
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-24 21:03:02 +0600

    comparison added

---
+++ fundamentals-concepts/comparison.js
@@ -0,0 +1,11 @@
+console.log("Two greater then Three: " , 2 > 3);
+console.log("Two less then Three: " , 2 < 3);
+console.log("Two and Three are Equals: " , 2 == 3);
+console.log("Three and Three are Equals: " , 3 == 3);
+console.log("Two and Three are not Equal: " , 2 != 3);
+console.log("Three and Three are not Equal: " , 3 != 3);
+
+var tomScore = 21;
+var jerryScore = 11;
+var compareOne = tomScore < jerryScore;
+console.log(compareOne);
