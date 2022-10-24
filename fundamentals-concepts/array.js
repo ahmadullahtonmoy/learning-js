60c339a4eb1d90cdf6d8d2b3d06ef0fb39a1b1e9
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-24 06:25:54 +0600

    array worked

--- fundamentals-concepts/array.js
+++ fundamentals-concepts/array.js
@@ -3,3 +3,7 @@ var foodNames = ["Biriyani","Chicken Fry","Chicken Chawmin","Pizza"];
 var foodPrices = [50, 30, 20, 15];
 
 var alphabet = ["A","B","C","D","E","F","G"];
+
+console.log(foodNames);
+console.log(foodPrices);
+console.log(alphabet);
