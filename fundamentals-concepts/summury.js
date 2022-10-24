b00bc8dafd168bde50fec01de6f31ec4b0abd014
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-24 13:06:36 +0600

    summury updated

--- fundamentals-concepts/summury.js
+++ fundamentals-concepts/summury.js
@@ -5,3 +5,7 @@
 // how to check array element index?
 // how to check array element by position?
 // how to change an array element from out side?
+// how to add element in array from last by using push?
+// how to remove element from last position of an array?
+// how to add an element in beginning of an array?
+// how to remove an element from beginning of an array?
