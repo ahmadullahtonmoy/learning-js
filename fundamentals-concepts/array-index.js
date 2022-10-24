c7b0cc2f73092e2ef4b7471521e42cc1ecd79113
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-24 12:40:10 +0600

    checking array element by index added

--- fundamentals-concepts/array-index.js
+++ fundamentals-concepts/array-index.js
@@ -4,3 +4,13 @@ var blackIndex = colors.indexOf("Black");
 var redIndex = colors.indexOf("Red");
 var blueIndex = colors.indexOf("Blue");
 console.log(whiteIndex , redIndex , blueIndex , blackIndex);
+
+// now i'm practicing checking element by index.
+var indexSix = colors[6];
+var indexFive = colors[5];
+var indexFour = colors[4];
+var indexThree = colors[3];
+var indexTwo = colors[2];
+var indexOne = colors[1];
+var indexZero = colors[0];
+console.log(indexSix , indexFive , indexFour , indexThree , indexTwo , indexOne , indexZero);
