4f164e9fc30d5250918bc63e7aaf184b2f6dede7
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-24 12:31:59 +0600

    array index added

---
+++ fundamentals-concepts/array-index.js
@@ -0,0 +1,6 @@
+var colors = ["White","Red","Blue","Green","Yellow","Purple","Black"];
+var whiteIndex = colors.indexOf("White");
+var blackIndex = colors.indexOf("Black");
+var redIndex = colors.indexOf("Red");
+var blueIndex = colors.indexOf("Blue");
+console.log(whiteIndex , redIndex , blueIndex , blackIndex);
