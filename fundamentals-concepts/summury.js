63db1ce087e9b440d4855756fd64d12ed0d5dd59
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-24 06:45:54 +0600

    summury updated

--- fundamentals-concepts/summury.js
+++ fundamentals-concepts/summury.js
@@ -1,2 +1,7 @@
 // how to declare an array?
 // how to check array length?
+// what is array index?
+// array index start with 0.
+// how to check array element index?
+// how to check array element by position?
+// how to change an array element from out side?
