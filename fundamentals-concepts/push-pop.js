c72a02deb2e17d7a86f14166bb959897543db78a
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-24 13:13:40 +0600

    added some element in the array last position by using pop

---
+++ fundamentals-concepts/push-pop.js
@@ -0,0 +1,13 @@
+var fruitsBucket = ["Apple","Orange"];
+console.log(fruitsBucket);
+
+// now i'm adding element in the last of the array 
+fruitsBucket.push("Banana");
+fruitsBucket.push("Lichi");
+fruitsBucket.push("Blue Berry");
+fruitsBucket.push("Dragon Fruit");
+fruitsBucket.push("Red Berry");
+fruitsBucket.push("Lemon");
+fruitsBucket.push("Water Melon");
+fruitsBucket.push("Mango");
+console.log(fruitsBucket);
