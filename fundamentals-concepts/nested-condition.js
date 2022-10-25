20be944cb34f76beb4e069b3cfc4c217da5237f5
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-25 13:03:06 +0600

    nedted condition added

---
+++ fundamentals-concepts/nested-condition.js
@@ -0,0 +1,14 @@
+var friedFish = 210;
+var packedWell = false;
+packedWell = true;
+
+var myBudget = 500;
+
+if (friedFish <= myBudget) {
+    if (packedWell == true) {
+        console.log("I'll buy Fried Fish.")
+    }
+    else{
+        console.log("I'll but Fried Chicken.")
+    }
+}
