cd84177b7ae5e48da4276c26c5d26c8c695c446b
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-10-28 14:33:35 +0600

    array.js updated

--- fundamentals-concepts/array.js
+++ fundamentals-concepts/array.js
@@ -4,6 +4,8 @@ var foodPrices = [50, 30, 20, 15];
 
 var alphabet = ["A","B","C","D","E","F","G"];
 
+var someName = ["RED","BLUE"];
+
 console.log(foodNames);
 console.log(foodPrices);
 console.log(alphabet.length);
