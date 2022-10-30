a6e4115e3523de5bab9fe24768e52ede80a72cb1
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-30 16:31:56 +0600

    while loop modified

--- fundamentals-concepts-recap-and-loop/while-loop.js
+++ fundamentals-concepts-recap-and-loop/while-loop.js
@@ -1,6 +1,6 @@
 var roastGiven = 0;
 while (roastGiven <= 8) {
-    console.log("Rost Den please");
+    console.log("Rost Den please", roastGiven);
     roastGiven++;
-    console.log(roastGiven);
+    // console.log(roastGiven);
 }
