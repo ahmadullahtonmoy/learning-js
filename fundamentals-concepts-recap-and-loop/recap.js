0c246de923168aad454abd8f92391ad263dcf31c
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-30 16:36:54 +0600

    array index added

--- fundamentals-concepts-recap-and-loop/recap.js
+++ fundamentals-concepts-recap-and-loop/recap.js
@@ -3,9 +3,12 @@
 
 // array
 var colors = ["White","Black","Blue","Red"];
-
+console.log(colors);
 // array index
-
+var blackIndex = colors.indexOf("Black");
+colors[2]= "Dark";
+console.log(colors);
+console.log(blackIndex);
 // push & pop
 
 // length
