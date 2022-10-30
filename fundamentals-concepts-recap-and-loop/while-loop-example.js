573841c684ba16ad0705e2d320c8945ef2b18768
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-31 00:13:09 +0600

    odd number with while loop added

--- fundamentals-concepts-recap-and-loop/while-loop-example.js
+++ fundamentals-concepts-recap-and-loop/while-loop-example.js
@@ -1,6 +1,13 @@
-// even evenNumber with while loop
+// even number with while loop
 var evenNumber = 0;
 while (evenNumber <= 20) {
     console.log(evenNumber);
     evenNumber = evenNumber + 2;
+}
+
+// odd number with while loop
+var oddNumber = 1;
+while (oddNumber <= 20) {
+    console.log(oddNumber);
+    oddNumber = oddNumber + 2;
 }
