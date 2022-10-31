74f6e297b66b8c9f41e6c51b1b20f26cdbcf9c8c
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-31 23:47:24 +0600

    fundamentals note updated

--- fundamentals-concepts/note.js
+++ fundamentals-concepts/note.js
@@ -1,4 +1,4 @@
-
+/* 
 1 Module Introduction and Variable recap
 2 Declare Array, array length and array index
 3 Array index, get and set by index, indexOf
@@ -8,3 +8,4 @@
 7 Handle multiple conditions, and or
 8 (advanced) Multi stage condition and nested conditions
 9 Module summary and two more comparisons
+ */
