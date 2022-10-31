af119cec0f73bf9c0a9913ed562acfd7b36bae5a
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-31 08:34:01 +0600

    odd number with for loop added

--- fundamentals-concepts-recap-and-loop/for-loop.js
+++ fundamentals-concepts-recap-and-loop/for-loop.js
@@ -1,9 +1,13 @@
 for (var i = 0; i <= 20; i++) {
-    console.log(i);
+    console.log("COUNTER",i);
 }
 
 // even number with for loop
 for (var i = 0; i <= 20; i = i + 2) {
-    console.log(i);
+    console.log("EVEN NUMBER",i);
 }
 
+// odd number with for loop
+for (var i = 1; i <= 15; i = i + 2) {
+    console.log("ODD NUMBER",i);
+}
