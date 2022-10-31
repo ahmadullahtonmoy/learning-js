820a77864ce980bc0ac33ebd61c99ff216cde843
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-10-31 23:36:32 +0600

    recap.js updated

---
+++ core-concept-function-object/recap.js
@@ -0,0 +1,18 @@
+// how to declare a variable?
+
+
+// how to declare an array?
+
+// how to check array element position with index?
+
+// how to push an element in array at last position?
+
+// how to pop an element from an array last position?
+
+
+// conditions (if else)
+
+
+// while loop
+
+// for loop
