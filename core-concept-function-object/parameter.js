86c1ad7932880ccf69b2ab1504aa715cde5db415
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-02 11:09:14 +0600

    funciton with return value added

--- core-concept-function-object/parameter.js
+++ core-concept-function-object/parameter.js
@@ -1,7 +1,11 @@
 function bringShingara (taka) {
     console.log("Singarar jonno dice", taka ,"Taka");
     console.log("mama singara den");
+    var singaraPrice = 10;
+    var giveSingara = taka / singaraPrice;
+    return giveSingara;
 }
 
 var money = 250;
-bringShingara(money);
+var returnShingara = bringShingara(money);
+console.log("Shingara Dice",returnShingara,"Ta");
