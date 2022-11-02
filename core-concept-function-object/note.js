3b6aea1e36ce16a633c82abce6ea1e2e6804e7d7
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-02 11:27:58 +0600

    note.js updated

--- core-concept-function-object/note.js
+++ core-concept-function-object/note.js
@@ -2,4 +2,5 @@
 1 Module Introduction and concept Recap
 2 Declare a Function, call function, function vs loop
 3 (advanced) Function parameter, function return
+4 (advanced) Multiple parameter add, multiplication, etc
 */
