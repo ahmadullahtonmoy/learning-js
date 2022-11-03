db542c61bcc7f12882f8c7cc1927830bdf82c4f5
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-03 10:31:37 +0600

    adding funtion added and it worked

---
+++ core-concept-function-object/multiple-parameter.js
@@ -0,0 +1,11 @@
+// this function will add two numbers as like calculator
+function addTwoNumbers (firstNumber , secondNumber) {
+    var addTotal = firstNumber + secondNumber;
+    return addTotal;
+}
+
+var priceOne = 15;
+var priceTwo = 20;
+
+var fuctionResult = addTwoNumbers(priceOne , priceTwo);
+console.log(fuctionResult);
