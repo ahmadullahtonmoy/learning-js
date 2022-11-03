7e27ff1b509e092ba00468edfe5b797cdb183712
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-03 12:40:19 +0600

    note updated of core concept

--- core-concept-function-object/note.js
+++ core-concept-function-object/note.js
@@ -3,4 +3,5 @@
 2 Declare a Function, call function, function vs loop
 3 (advanced) Function parameter, function return
 4 (advanced) Multiple parameter add, multiplication, etc
+5 Declare multiple objects with multiple properties
 */
