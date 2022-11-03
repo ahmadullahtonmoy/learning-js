b5954fec54476d58a1c1b3c2957b9c9706a1fdbd
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-03 10:54:45 +0600

    subtraction function added to calculator.js and it worked well

--- core-concept-function-object/calculator.js
+++ core-concept-function-object/calculator.js
@@ -11,3 +11,18 @@ var additionText = "This is Addition Result";
 
 var additionResult = addition(priceOne , priceTwo);
 console.log(additionText,additionResult);
+
+
+// subtractions function
+function subtraction (firstSubtractNumber,secondSubtractNumber) {
+    var totalSubtraction = firstSubtractNumber - secondSubtractNumber;
+    return totalSubtraction;
+}
+
+var budgetMoney = 200;
+var totalAmount = 170;
+
+var subtractionText = "This is Subtraction Result";
+
+var subtractionResult = subtraction(budgetMoney,totalAmount);
+console.log(subtractionText,subtractionResult);
