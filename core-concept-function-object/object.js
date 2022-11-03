a339d83ecd9e89964d24607906df8564aca582ce
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-04 02:37:30 +0600

    an object replaced with another object

--- core-concept-function-object/object.js
+++ core-concept-function-object/object.js
@@ -1,9 +1,8 @@
-var ariba = {
-    age: 12,
-    color: "Black",
-    husbend:"Enayet Ullah",
-    class: 1,
-    shoshur: "Gulu Mulu miya",
+var computer = {
+    processor: "Core i5 10th Gen",
+    ram: "16 GB",
+    rom: "512 SSD",
+    color: "Black"
 }
 
-console.log(ariba);
+console.log(computer);
