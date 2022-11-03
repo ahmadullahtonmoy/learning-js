6f28e991e74a41bca607ae89d065a8e1a27e358d
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-03 10:43:18 +0600

    additonal function added and it worked

---
+++ core-concept-function-object/calculator.js
@@ -0,0 +1,13 @@
+// additional function
+function addition (firstAdditionNumber , secondAdditionNumber) {
+    var totalAddition = firstAdditionNumber + secondAdditionNumber;
+    return totalAddition;
+}
+
+var priceOne = 25;
+var priceTwo = 15;
+
+var additionText = "This is Addition Result";
+
+var additionResult = addition(priceOne , priceTwo);
+console.log(additionText,additionResult);
