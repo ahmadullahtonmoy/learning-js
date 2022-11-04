6a1438cd1034b9caa92d065dd995c674d140928a
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-04 09:59:05 +0600

    summary and note.txt updated

--- core-concept-function-object/summary.js
+++ core-concept-function-object/summary.js
@@ -1,11 +1,9 @@
-// how to declare a function
-
-// giving a name of function
-
-// how to call a function
-
-// how to set parameter in function
-
-// how to use return in function
-
-// how to use multiple parameter in function
+// 01 - how to declare a function
+// 02 - giving a name of function
+// 03 - how to call a function
+// 04 - how to set parameter in function
+// 05 - how to use return in function
+// 06 - how to use multiple parameter in function
+// 07 - how to declare an object
+// 08 - how to see a property of an object
+// 09 - there is two way to see an object property
