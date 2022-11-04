219e598372b6a8298f98b60545cae680d7f0fb9b
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-04 12:11:20 +0600

    a switch added

---
+++ core-concept-function-object/switch.js
@@ -0,0 +1,19 @@
+var color = "Green";
+
+switch (color) {
+    case "Red":
+        console.log("Red");
+        break;
+    case "Green":
+        console.log("This is Green");
+        break;
+    case "Blue":
+        console.log("This is Blue");
+        break;
+    case "Gray":
+        console.log("This is Gray");
+        break;
+    case "Blue Violate":
+        console.log("This is Blue Violate");
+        break;
+}
