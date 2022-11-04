74607fe6d6e9909cee8341204d5970a2f9e5afb1
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-04 10:09:35 +0600

    an obeject declared

---
+++ core-concept-function-object/object-property.js
@@ -0,0 +1,11 @@
+var mobileOne = {
+    Brand: "Samsung",
+    Model: "J6 2018",
+    RAM: "3-GB",
+    ROM: "32-GB",
+    Release: "2018",
+    Price: "27,500/= taka",
+};
+
+console.log(mobileOne);
+
