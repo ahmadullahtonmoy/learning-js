36692488fcf6735c7baf7cfbc6b4346661548391
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-10 14:24:26 +0600

    a fuction declared

---
+++ apply-javaScript-concept/inch-to-feet.js
@@ -0,0 +1,8 @@
+function bringSingara(taka){
+    let singaraPrice = 5;
+    let totalSingara = taka / singaraPrice;
+    return totalSingara;
+}
+
+var singaraQuantity = bringSingara(50);
+console.log("Shingara Quantity:",singaraQuantity);
