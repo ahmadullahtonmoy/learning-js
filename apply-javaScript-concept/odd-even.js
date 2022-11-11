0a57ad002ef4c16966d56f783f39f47678e5c2d2
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-11 18:14:39 +0600

    an odd number checking function declared

--- apply-javaScript-concept/odd-even.js
+++ apply-javaScript-concept/odd-even.js
@@ -7,7 +7,20 @@ function isEven (number) {
     return false;
 }
 
-const evenConsoleText = "20 is even number:";
+const evenConsoleText = "The Input Number is even number:";
 
 const isEvenCheck = isEven(20);
 console.log(evenConsoleText,isEvenCheck);
+
+
+function isOdd (numberOdd) {
+    if (numberOdd % 2 != 0) {
+        return true;
+    }
+    return false;
+}
+
+const oddConsoleText = "The Input Number is odd Number:";
+
+const isOddCheck = isOdd(20);
+console.log(oddConsoleText,isOddCheck);
