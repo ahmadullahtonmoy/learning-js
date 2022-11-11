04673de1bafc406f44c0308f8ab143fad50f6ae0
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-11 18:34:38 +0600

    notes updated

--- apply-javaScript-concept/note.js
+++ apply-javaScript-concept/note.js
@@ -2,4 +2,5 @@
 1 Module Introduction Apply JS and let, const
 2 Unit Convert Inch to Feet, miles to kilometer
 3 Check even and odd number using function
+4 Check whether a year is a Leap Year or not
 */
