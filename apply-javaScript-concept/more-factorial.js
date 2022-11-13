2ae450720349c1d89667c8fbeb3bed538a8f195e
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-13 09:21:52 +0600

    facorial loop in function added

---
+++ apply-javaScript-concept/more-factorial.js
@@ -0,0 +1,24 @@
+let factorialOne = 1;
+let counterOne = 4;
+
+for (let i = 1; i <=counterOne; i++) {
+    factorialOne = factorialOne * i;
+}
+
+console.log("This is Factorial One Result:",factorialOne);
+
+
+// factorial loop with function
+
+function factorialLoop (numberOne) {
+    let factorialTwo = 1;
+    for (let i = 1; i <= numberOne; i++) {
+        factorialTwo = factorialTwo * i;
+    }
+    return factorialTwo;
+}
+
+let randomNumbers = 5;
+
+const callFactorialFunction = factorialLoop(randomNumbers);
+console.log("Factorial of",randomNumbers,"is =",callFactorialFunction);
