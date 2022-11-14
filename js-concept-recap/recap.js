63ce5ffa85db58d00ed0189582e501bdc23d149a
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-14 23:33:31 +0600

    notes added

---
+++ js-concept-recap/recap.js
@@ -0,0 +1,18 @@
+/*
+1 Javascript concepts and apply revision with four challenges
+*/
+
+// core concepts
+// variable (let,const) javascript var, let , const
+// array, loop (while,for), conditionals, function
+// leap year
+// odd even
+// factorial (for while)
+
+/*
+home work
+1- celcious to farenheight (function)
+2- reverse of prev.
+3- grade calculator
+4- simple interest
+*/
