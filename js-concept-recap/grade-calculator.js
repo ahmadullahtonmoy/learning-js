631687575a8f0838609074fb4cd4dd18a7a35c72
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-15 23:21:37 +0600

    grade calculator added and launch.json file added

---
+++ js-concept-recap/grade-calculator.js
@@ -0,0 +1,32 @@
+
+function getGrade (num) {
+    if (num >= 80 && num <= 100) {
+        console.log("Yey!! You Got GPA-5!");
+    }
+    else if (num >= 70 && num <= 79) {
+        console.log("You Got A");
+    }
+    else if (num >= 60 && num <= 69) {
+        console.log("You Got A-");
+    }
+    else if (num >= 50 && num <= 59) {
+        console.log("You Got B");
+    }
+    else if (num >= 40 && num <= 49) {
+        console.log("You Got C");
+    }
+    else if (num >= 33 && num <= 39) {
+        console.log("You Got A");
+    }
+    else if (num >= 0 && num <= 32) {
+        console.log("Ohh NO!! You Are Failed!");
+    }
+}
+
+getGrade(90);
+getGrade(70);
+getGrade(60);
+getGrade(50);
+getGrade(40);
+getGrade(33);
+getGrade(32);
