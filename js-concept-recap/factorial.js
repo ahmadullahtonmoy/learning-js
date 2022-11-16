2d801e0e4dd1330b1b36961640f67cd1050ae50f
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-16 07:10:09 +0600

    factorial whith while added

--- js-concept-recap/factorial.js
+++ js-concept-recap/factorial.js
@@ -1,4 +1,4 @@
-
+// factorial function with for loop
 function getFactorial (num) {
     let factCount = 1;
     for (let i = 1; i <= num; i++) {
@@ -9,3 +9,18 @@ function getFactorial (num) {
 
 const getFact = getFactorial(5);
 console.log(getFact);
+
+
+// factorial function with while loop
+function whileFactorial (number) {
+    let factorialCounter = 1;
+    let loopCounter = 1;
+    while (loopCounter <= number) {
+        factorialCounter = factorialCounter * loopCounter;
+        loopCounter++;
+    }
+    return factorialCounter;
+}
+
+const whileFact = whileFactorial(5);
+console.log(whileFact);
