07da4d98665178ee1a9ffd75ff6c8bc744b631fb
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-20 23:53:00 +0600

    swap with variable added

--- java-script-simple-coding-problems/swap.js
+++ java-script-simple-coding-problems/swap.js
@@ -1,4 +1,15 @@
 let colorOne = "Black";
 let colorTwo = "White";
 
-console.log(colorOne);
+console.log(colorOne,colorTwo);
+
+// first approach
+let temp = colorOne;
+colorOne = colorTwo;
+colorTwo = temp;
+
+console.log(colorOne,colorTwo);
+
+// second approach
+[colorOne,colorTwo] = [colorTwo,colorOne];
+console.log(colorOne,colorTwo);
