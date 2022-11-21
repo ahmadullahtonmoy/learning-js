5ad1ece51065358a7456a97fbe049646b9a86509
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-21 08:30:36 +0600

    updated

--- java-script-simple-coding-problems/math.js
+++ java-script-simple-coding-problems/math.js
@@ -2,3 +2,11 @@ let fractionalNumber = 10.10;
 let number = 11;
 
 console.log(Math.floor(fractionalNumber));
+console.log(Math.ceil(fractionalNumber));
+console.log(Math.random(number));
+console.log(Math.round(number));
+
+
+let diceP = Math.random(number);
+let dice = Math.round(diceP);
+console.log(dice);
