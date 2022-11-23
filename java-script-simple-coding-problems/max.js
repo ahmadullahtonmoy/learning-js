f483f86d6555302756c6c50c78244945e0db799a
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-23 23:37:40 +0600

    notes updated

---
+++ java-script-simple-coding-problems/max.js
@@ -0,0 +1,2 @@
+// Task - 1: create a function with three parameter, that's find for max valued item.
+// Task - 2: create a function, that's find lowest valued item.
--- java-script-simple-coding-problems/note.js
+++ java-script-simple-coding-problems/note.js
@@ -2,4 +2,5 @@
 1 Module Introduction, Math and Random number
 2 Swap variable, swap without temp, destructing
 3 Find max of two values, find max of three values
+4 Sum of all numbers in an array
 */
