90675559d3f72d09f2c859362f6e9b0e593887a8
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-24 08:00:52 +0600

    find max value machine function added

--- java-script-simple-coding-problems/max.js
+++ java-script-simple-coding-problems/max.js
@@ -1,2 +1,24 @@
 // Task - 1: create a function with three parameter, that's find for max valued item.
 // Task - 2: create a function, that's find lowest valued item.
+
+let max = Math.max(5,10);
+console.log(max);
+
+function findMax (first,second,third) {
+    if (first > second && first > third) {
+        return first;
+    }
+    else if (second > first && second > third) {
+        return second;
+    }
+    else if (third > first && third > second) {
+        return third;
+    }
+}
+
+let apple = 100;
+let orange = 200;
+let lichi = 300;
+
+let biggerNumber = findMax(apple,orange,lichi);
+console.log(biggerNumber,"is Bigger than Others");
