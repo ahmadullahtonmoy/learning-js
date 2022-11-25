671df3bf3d6ca972de58bf2c6c4239ceedb2af95
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-25 22:41:37 +0600

    notes updated

--- java-script-simple-coding-problems/note.js
+++ java-script-simple-coding-problems/note.js
@@ -3,6 +3,7 @@
 2 Swap variable, swap without temp, destructing
 3 Find max of two values, find max of three values
 4 Sum of all numbers in an array
+5 Find the largest element of an array
 */
 
 // is object can up-down variable value?
