8f069a636b9a03bb45ef4bc23276a43b223605ea
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-26 17:19:10 +0600

    some variables added

---
+++ notes/variables.js
@@ -0,0 +1,12 @@
+// declare a variable in random way:
+
+let number;
+number = 12;
+
+let numOne , numTwo , numThree;
+
+numOne = 2; numTwo = 4; numThree = 6;
+
+const sumOfAll = numOne + numTwo + numThree;
+
+console.log(sumOfAll);
