d01b954082bad3edaae49520b0816796165176ad
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-30 00:56:38 +0600

    simple math added

--- notes/operators.js
+++ notes/operators.js
@@ -4,3 +4,10 @@ applePrice = 5; bananaPrice = 10; watermelonPrice = 20;
 // addition of those values:
 let totalPrice = applePrice + bananaPrice + watermelonPrice;
 console.log("All Fruits Total Price is =",totalPrice,"Taka");
+
+// increment test:
+console.log("Before Increment Apple Price is=",applePrice,"Taka");
+applePrice ++;
+console.log("After Increment one step by '++' Apple Price is=",applePrice,"Taka");
+applePrice += 4;
+console.log("After Increment Four by '+= 4' Apple Price is=",applePrice,"Taka");
