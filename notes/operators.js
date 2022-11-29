0f680f93040a7cf22c2df80d2e64b3ccbcc109eb
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-30 00:06:32 +0600

    addition added

--- notes/operators.js
+++ notes/operators.js
@@ -1,2 +1,6 @@
 let applePrice; let bananaPrice; let watermelonPrice;
-applePrice = 5;
+applePrice = 5; bananaPrice = 10; watermelonPrice = 20;
+
+// addition of those values:
+let totalPrice = applePrice + bananaPrice + watermelonPrice;
+console.log("All Fruits Total Price is =",totalPrice,"Taka");
