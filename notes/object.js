909b66c8790ffa3b88f13b212f59e0d205ee4d58
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-30 07:54:33 +0600

    an object named laptop declared

---
+++ notes/object.js
@@ -0,0 +1,11 @@
+const laptopOne = {
+    brandName : "HP",
+    model : "HP EliteBook 840 G5",
+    os : "Windows 11",
+    processor : "intel Core i5 8th Gen",
+    ram : "16gb",
+    ssd : "512",
+    price : "$20",
+};
+
+console.log(laptopOne.brandName);
