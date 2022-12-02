5671f25b74a5b0c42f6b98c7cbaab17a3301146c
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-02 23:31:22 +0600

    up down with ceil and floor

--- notes/math.js
+++ notes/math.js
@@ -6,3 +6,12 @@ console.log(intOne);
 // random numbers
 let random = Math.random();
 console.log(random);
+
+// integer to upper closest number
+let numberUpDown = 5.7;
+let intTwo = Math.ceil(numberUpDown);
+console.log(intTwo);
+
+// integer to lower closest number
+let intThree = Math.floor(numberUpDown);
+console.log(intThree);
