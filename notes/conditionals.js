62dc3e3171cc5932e4bdd28d4fb0c5ecbd9bf6ee
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-03 14:54:31 +0600

    condition added

---
+++ notes/conditionals.js
@@ -0,0 +1,8 @@
+// greater then , less then , equals to ......... ETC
+let apple; let orange; let banana;
+
+apple = 10; orange = 20; banana = 30;
+
+if (apple < orange) {
+    console.log("Take Apple");
+}
