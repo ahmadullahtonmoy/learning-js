83a553b62846624b9a44a1a0912ec476da15c5dd
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-05 01:01:46 +0600

    here i'm back

--- notes/conditionals.js
+++ notes/conditionals.js
@@ -16,17 +16,18 @@ switch off , good condition.
 switch off , bad condition.
 [out of topic{those are binary(1000 , 1011)those are binary}out of topic]
 */
-let activeSwitch = 9;
-let inactiveSwitch = 7;
+let activeSwitch = 1;
+let inactiveSwitch = 0;
 const deactivated = false;
 
-if (activeSwitch == 9 && inactiveSwitch == 7 && deactivated == false) {
+if (activeSwitch == 1 && inactiveSwitch == 0 && deactivated == false) {
     console.log("ON");
     console.log("Condition of line is Good!");
 }
-else if (inactiveSwitch == 7 && activeSwitch == 9 && deactivated == true) {
+else if (inactiveSwitch == 0 && activeSwitch == 1 && deactivated == true) {
     console.log("OFF");
     console.log("Condition of line is Good!");
 } else {
     console.log("The Power Line id DAMAGED!!!");
 }
+
