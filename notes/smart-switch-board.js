de4d44ebfd7d76cfd9c23ab124d7772d11e7c8d9
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-05 07:42:33 +0600

    switch activated conditions comment out

---
+++ notes/smart-switch-board.js
@@ -0,0 +1,8 @@
+/*
+switch activated {
+    positive(1) + negative(0) = health Good!
+    positive(1) + negative(!=0) = Error! Negative(1) Power on. But Health is not Good!
+    positive(0) + negative(0) = Error! Positive(0) , Negative(0). There is no Power! Please Check Your Power Management!
+    positive(!= 1) + negative(!=0) = Error! Positive(>~<) , Negative(>~<). Power Error!!! Check Out What Happened!
+}
+*/
