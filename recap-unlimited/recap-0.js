f5e163a9da1c6fe9dfd0cba086cf45970c668177
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-19 12:13:16 +0600

    json file configed and adding sum added

--- recap-unlimited/recap-0.js
+++ recap-unlimited/recap-0.js
@@ -5,9 +5,17 @@ let dressPrice = 20;
 // changing the value of variables
 dressPrice = 27;
 ricePrice = 20;
-someoneName = "Hablu 0.0"
+someoneName = "Hablu 0.0";
 
 // Declaring a constant, which value never can be change.
 const peopleName = "Rashid";
 const skyQty = 7;
 const humanMortal = true;
+
+// simply mathematics like adding , subtraction , multiply , divide with var or let.
+let iceCreamPrice = 20;
+let chocolatePrice = 25;
+let donutPrice = 30;
+
+var addingSum = iceCreamPrice + chocolatePrice + donutPrice;
+console.log(addingSum);
