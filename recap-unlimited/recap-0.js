721082260f57f7f8cb7ca63ff55882512e18c6d5
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-19 11:36:58 +0600

    how to declare a variable and change a value of it

---
+++ recap-unlimited/recap-0.js
@@ -0,0 +1,8 @@
+// Declaring a variable
+var ricePrice = 15;
+var someoneName = "Hablu";
+let dressPrice = 20;
+// changing the value of variables
+dressPrice = 27;
+ricePrice = 20;
+someoneName = "Hablu 0.0"
