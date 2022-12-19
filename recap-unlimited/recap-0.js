2969587054d026c9fd2865cd8b9cb3c0a87ec82b
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-12-19 12:23:00 +0600

    donut price find out with subtraction

--- recap-unlimited/recap-0.js
+++ recap-unlimited/recap-0.js
@@ -19,3 +19,7 @@ let donutPrice = 30;
 
 var totalPrice = iceCreamPrice + chocolatePrice + donutPrice;
 console.log("Total Price of Ice Cream , Chocolate and Donut is:" , totalPrice);
+
+// now i'll find out the donut price from the total price of those three item using subtraction.
+let knowDonutPrice = totalPrice - (iceCreamPrice + chocolatePrice);
+console.log("The Donut Price Is:" , knowDonutPrice)
