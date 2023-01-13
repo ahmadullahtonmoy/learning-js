ecf48404b05def5a2ae8ecfa95592f7c329931ab
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2023-01-14 00:01:29 +0600

    updated

--- recap-unlimited/recap-0.js
+++ recap-unlimited/recap-0.js
@@ -108,24 +108,3 @@ else {
     console.log("Khawa lagbona karo");
 }
 
-
-// for loop
-for(let i = 0; i <= 9; i++){
-    console.log(i);
-}
-
-// while loop
-let i = 0;
-while (i < 9) {
-    console.log(i);
-    i++;
-}
-
-
-
-// function
-function turnOn() {
-    console.log("Turn on the fan.");
-}
-
-turnOn();
