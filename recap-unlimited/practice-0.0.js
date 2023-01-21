e6d454e36a0bf38c8df59fbdcf068b8f152e71c1
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2023-01-21 09:00:16 +0600

    note txt and practice 0.0 js file with code and node added

---
+++ recap-unlimited/practice-0.0.js
@@ -0,0 +1,15 @@
+// task no-1 make a function that convert radian to degree
+// task no-2 make a function that will check is the given file is js file or not
+// task no-3 make a function that will return a addition result of three number
+// task no-4 make a function that will return for rest people local bus fare
+// task no-5 make a function that will return true or false of is best friend
+
+// 1 rad = 57.2958 deg
+
+function radToDeg (radian) {
+    const formula = radian * 57.2958;
+    return formula;
+}
+
+let getDeg = radToDeg(2);
+console.log(getDeg);
